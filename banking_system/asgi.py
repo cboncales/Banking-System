808142d19d2560534f252153d55b7ba557a5db74
cboncales <clarkboncales115@gmail.com>
2024-12-25 22:08:05 +0800

    added django channels
    Co-Authored-By: Lluvido <166703177+lluvido@users.noreply.github.com>
    Co-Authored-By: jzcheng <145268468+chirrelyn@users.noreply.github.com>

--- banking_system/asgi.py
+++ banking_system/asgi.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banking_system.settings')
 
 application = get_asgi_application()
+   
